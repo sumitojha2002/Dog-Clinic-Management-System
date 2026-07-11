89dcd973b7860799879a91aba51640d5d93d17e0
Sumit Ojha <102379532+sumitojha2002@users.noreply.github.com>
2026-07-11 22:46:36 +0545

    ui[frontend] home page ui added images, blog & about me

--- frontend/apps/owner-portal/src/pages/Home.tsx
+++ frontend/apps/owner-portal/src/pages/Home.tsx
@@ -1,13 +1,19 @@
 import { Button } from "../components/ui/button";
 import vetone from "../assets/vet1-removebg-preview.png";
 import vettwo from "../assets/vet2-removebg-preview.png";
-import { FlaskConical } from "lucide-react";
+import { ArrowUpRight, FlaskConical } from "lucide-react";
 import ProductCard from "../components/cards/ProductCard";
 import VetCard from "../components/cards/VetCard";
 import { useQuery } from "@tanstack/react-query";
 import AboutDog from "../../src/assets/AboutImageDog.jpg";
 import BirthdayBoi from "../../src/assets/birthdayboi.jpg";
 import { getVetsCardForHome } from "../services/api/authapi";
+import VetMedia from "../../src/assets/vetMedia.jpg";
+import VetMedia2 from "../../src/assets/vetMedia2.jpg";
+import VetMedia3 from "../../src/assets/vetMedia3.jpg";
+import VetMedia4 from "../../src/assets/vetMedia4.jpg";
+import VetMedia5 from "../../src/assets/vetMedia5.jpg";
+import DogDrinkArticle from "../../src/assets/dogdrinkarticle.jpg";
 
 function Home() {
   const { data } = useQuery({
@@ -162,7 +168,7 @@ function Home() {
         </div>
       </div>
 
-      <div className=" mt-30 mb-20 grid grid-cols-2 gap-2">
+      <div className=" mt-30 mb-20 grid grid-cols-2 gap-2  ">
         <div className="flex flex-col justify-between">
           <img
             src={AboutDog}
@@ -207,6 +213,110 @@ function Home() {
         <div className="grid grid-cols-3 gap-5 pl-10 pr-10">
           {data?.data &&
             data.data.map((vet) => <VetCard key={vet.vetId} {...vet} />)}
+        </div>
+      </div>
+
+      <div className="w-full mt-40">
+        <div>
+          <h1 className="text-4xl mb-10 font-semibold">Follow Us</h1>
+        </div>
+        <div className="grid h-200 auto-rows-fr grid-cols-3 w-full  gap-2">
+          <div className="grid grid-rows-2 w-full gap-3 h-full">
+            <img
+              src={VetMedia3}
+              className="border h-full w-full rounded-2xl object-cover min-h-0"
+            />
+            <img
+              src={VetMedia2}
+              className="border h-full  w-full rounded-2xl object-cover min-h-0"
+            />
+          </div>
+
+          <img
+            src={VetMedia}
+            className=" w-full border h-full rounded-2xl object-cover"
+          />
+
+          <div className="grid grid-rows-2 gap-3 w-full">
+            <img
+              src={VetMedia4}
+              className="border h-full w-full rounded-2xl object-cover"
+            />
+            <img
+              src={VetMedia5}
+              className="border  h-full w-full rounded-2xl object-cover"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full mt-30 mb-20 pl-5 pr-5">
+        <div>
+          <div className=" flex justify-between items-center">
+            <h1 className="text-4xl mt-10 mb-10 font-semibold">
+              Browse articles & news
+            </h1>
+            <Button variant={"ghost"}>Browse all post</Button>
+          </div>
+
+          <div className="lg:grid lg:grid-cols-2 gap-3 md:grid mb:grid-cols-2 sm:grid sm:grid-cols-1 grid grid-cols-1">
+            <div>
+              <div className="relative">
+                <img
+                  src={DogDrinkArticle}
+                  alt=""
+                  className="rounded-2xl h-100 object-cover"
+                />
+                <div className="absolute bottom-10 left-1/2 -translate-x-1/2  w-100 h-40 sm:w-100 sm:h-40 md:w-110 md:h-40 lg:w-120  bg-white rounded-2xl p-5 flex flex-col justify-between">
+                  <div className=" flex items-center gap-2">
+                    <div className="flex items-center gap-2 border pl-2 pr-2 rounded-2xl">
+                      <div className="bg-[#ffa0ea] rounded-full w-3 h-3 "></div>
+                      <h1 className="text-[15px] font-bold">Resources</h1>
+                    </div>
+                    <div className="text-[10px]">Jan 28 2026</div>
+                  </div>
+                  <div className="flex">
+                    <h1 className="text-start text-[21px] font-medium w-2/3">
+                      Senior pet care: how to keep aging pets comfortable
+                    </h1>
+                    <div className="flex w-1/3 justify-end items-end">
+                      <div className="p-2 border border-[#6f7173] rounded-full">
+                        <ArrowUpRight className="cursor-pointer" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="relative">
+                <img
+                  src={DogDrinkArticle}
+                  alt=""
+                  className="rounded-2xl h-100 object-cover"
+                />
+                <div className="absolute bottom-10 left-1/2 -translate-x-1/2  w-100 h-40 sm:w-100 sm:h-40 md:w-110 md:h-40 lg:w-120  bg-white rounded-2xl p-5 flex flex-col justify-between">
+                  <div className=" flex items-center gap-2">
+                    <div className="flex items-center gap-2 border pl-2 pr-2 rounded-2xl">
+                      <div className="bg-[#ffa0ea] rounded-full w-3 h-3"></div>
+                      <h1 className="text-[15px] font-bold">Resources</h1>
+                    </div>
+                    <div className="text-[10px]">Jan 28 2026</div>
+                  </div>
+                  <div className="flex">
+                    <h1 className="text-start text-[21px] font-medium w-2/3">
+                      Senior pet care: how to keep aging pets comfortable
+                    </h1>
+                    <div className="flex w-1/3 justify-end items-end">
+                      <div className="p-2 border border-[#6f7173] rounded-full">
+                        <ArrowUpRight className="cursor-pointer" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
