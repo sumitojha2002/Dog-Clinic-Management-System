5fc025861a4d7e124925c21f8d603fb83ef93d9d
Sumit Ojha <102379532+sumitojha2002@users.noreply.github.com>
2026-07-21 13:45:16 +0545

    feat[backend:SubCategory] all required services created.

--- backend/src/main/java/com/example/backend/repository/ecommers/SubCategoryRepositroy.java
+++ backend/src/main/java/com/example/backend/repository/ecommers/SubCategoryRepositroy.java
@@ -1,9 +1,18 @@
 package com.example.backend.repository.ecommers;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.backend.entity.ecommers.SubCategory;
 
 public interface SubCategoryRepositroy extends JpaRepository<SubCategory,Long> {
-    
+    @Query("""
+            SELECT s FROM
+            SubCategory s
+            WHERE s.category.id = id
+            """)
+    List<SubCategory> getAllSubCategoriesByCatId(@Param("id") Long id); 
 }
--- backend/src/main/java/com/example/backend/services/ecommers/SubCategoryService.java
+++ backend/src/main/java/com/example/backend/services/ecommers/SubCategoryService.java
@@ -1,6 +1,7 @@
 package com.example.backend.services.ecommers;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import com.example.backend.entity.ecommers.dto.CreateSubCategoryDTO;
 import com.example.backend.repository.ecommers.SubCategoryRepositroy;
 import com.example.backend.response.Response;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -45,4 +47,80 @@ public class SubCategoryService {
             return Response.ResponseHandler(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @Transactional
+    public ResponseEntity<?> updateSubCategory(CreateSubCategoryDTO createSubCategoryDTO,Long id){
+        try{
+            Optional<SubCategory> subCatOptional = subCategoryRepo.findById(id);
+
+            if(!subCatOptional.isPresent()){
+                return Response.ResponseHandler(HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND);
+            }
+            SubCategory subCategory = subCatOptional.get();
+
+            if(!createSubCategoryDTO.getName().isEmpty()){
+                subCategory.setName(createSubCategoryDTO.getName());
+            }
+
+            if(!createSubCategoryDTO.getDescription().isEmpty()){
+                subCategory.setDescription(createSubCategoryDTO.getDescription());
+            }
+            
+            return Response.ResponseHandler(HttpStatus.OK.getReasonPhrase(), HttpStatus.OK,subCategory);
+
+        }catch(Exception e){
+            e.printStackTrace();
+            return Response.ResponseHandler(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    public ResponseEntity<?> getAllSubCategoriesByCategoryId(Long id){
+        try{
+            List<SubCategory> subCategories = subCategoryRepo.getAllSubCategoriesByCatId(id);
+
+            if(subCategories.isEmpty()){
+                return Response.ResponseHandler("Empty there is no sub categories.", HttpStatus.NOT_FOUND);
+            }
+
+            return Response.ResponseHandler(HttpStatus.FOUND.getReasonPhrase(), HttpStatus.FOUND,subCategories);
+
+        }catch(Exception e){
+            e.printStackTrace();
+            return Response.ResponseHandler(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<?> getSubCategoryById(Long id){
+        try{
+            Optional<SubCategory> subCategory = subCategoryRepo.findById(id);
+
+            if(!subCategory.isPresent()){
+                return Response.ResponseHandler(HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND);
+            }
+
+            return Response.ResponseHandler(HttpStatus.OK.getReasonPhrase(),HttpStatus.OK,subCategory);
+        }catch(Exception e){
+            e.printStackTrace();
+            return Response.ResponseHandler(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Transactional
+    public ResponseEntity<?> deleteBySubCatId(Long id){
+        try{    
+            Optional<SubCategory> subCategory = subCategoryRepo.findById(id);
+
+            if(!subCategory.isPresent()){
+                return Response.ResponseHandler(HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND);
+            }
+
+            subCategoryRepo.delete(subCategory.get());
+            return Response.ResponseHandler(HttpStatus.OK.getReasonPhrase(), HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+            return Response.ResponseHandler(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), HttpStatus.INSUFFICIENT_STORAGE);
+        }
+    }
+
 }
